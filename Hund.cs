0542d89bc3cce01ce362d4b1a615557338fc235d
christian <christian.ulstein@gmail.com>
2025-05-21 12:14:54 +0200

    Made a virtual pet generator that has actions like feed, pet, gototoilet

--- Hund.cs
+++ Hund.cs
@@ -1,8 +1,36 @@
 namespace Virtual_pet;
 
-public class VirtualPet
+public class Hund
 {
-    private string name;
     private int age;
     private int hunger;
+    private int happiness;
+    private int bladder;
+
+    public Hund()
+    {
+        Random rnd = new Random();
+        age = rnd.Next(1, 16);
+        hunger = rnd.Next(1, 11);
+        happiness = rnd.Next(1, 11);
+        bladder = rnd.Next(1, 11);
+    }
+    public string GetStatus()
+    {
+        return $"Alder: {age}, Sult: {hunger}, Lykke: {happiness}, Blære: {bladder}";
+    }
+    public void Feed()
+    {
+        hunger--;
+    }
+
+    public void Pet()
+    {
+        happiness++;
+    }
+
+    public void GoToToilet()
+    {
+        bladder--;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,69 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Velg kjæledyr");
+        Console.WriteLine("Hund");
+        Console.WriteLine("Katt");
+        
+        string valg = Console.ReadLine();
+        switch (valg)
+        {
+            case "Hund":
+                Hund hund = new Hund();
+                SelectedPet(hund);
+                break;         
+            case "Katt":
+                Katt katt = new Katt();
+                Console.WriteLine(katt.GetStatus());
+                SelectedPet(katt);
+                break;
+            default: 
+                Console.WriteLine("Ikke gjylding");
+                break;
+        }
+        
+    }
+    
+    static void SelectedPet(object pet)
+    {
+        Console.WriteLine("Hva vil du gjøre? (Feed, Pet, GoToToilet)");
+        string command = Console.ReadLine();
+        if (pet is Katt katt)
+        {
+            switch (command)
+            {
+                case "Feed":
+                    katt.Feed();
+                    Console.WriteLine(katt.GetStatus());
+                    break;
+                case "Pet":
+                    katt.Pet();
+                    break;
+                case "GoToToilet":
+                    katt.GoToToilet();
+                    break;
+                default: 
+                    Console.WriteLine("Ikke gjylding");
+                    break;
+            }
+        }
+        else if (pet is Hund hund)
+        {
+            switch (command)
+            {
+                case "Feed":
+                    hund.Feed();
+                    break;
+                case "Pet":
+                    hund.Pet();
+                    break;
+                case "GoToToilet":
+                    hund.GoToToilet();
+                    break;
+                default: 
+                    Console.WriteLine("Ikke gjylding");
+                    break;
+            }
+        }
     }
 }
