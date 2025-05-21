35cfdb6c2109190922ab14cee89b1f732ff48dee
christian <christian.ulstein@gmail.com>
2025-05-21 13:16:14 +0200

    added more pets and made it run indefinetely until user stops it

--- Program.cs
+++ Program.cs
@@ -4,68 +4,147 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Velg kjæledyr");
-        Console.WriteLine("Hund");
-        Console.WriteLine("Katt");
-        
-        string valg = Console.ReadLine();
-        switch (valg)
+        bool programRunning = true;
+        while (programRunning)
         {
-            case "Hund":
-                Hund hund = new Hund();
-                SelectedPet(hund);
-                break;         
-            case "Katt":
-                Katt katt = new Katt();
-                Console.WriteLine(katt.GetStatus());
-                SelectedPet(katt);
-                break;
-            default: 
-                Console.WriteLine("Ikke gjylding");
-                break;
+            Console.WriteLine("Velg kjæledyr");
+            Console.WriteLine("Hund");
+            Console.WriteLine("Katt");
+            Console.WriteLine("Pappegøye");
+            Console.WriteLine("Ørn");
+            Console.WriteLine("For å avslutte skriv 'Avslutt'");
+            string valg = Console.ReadLine();
+            
+            switch (valg)
+            {
+                case "Hund":
+                    Hund hund = new Hund();
+                    Console.WriteLine(hund.GetStatus());
+                    SelectedPet(hund);
+                    break;
+                case "Katt":
+                    Katt katt = new Katt();
+                    Console.WriteLine(katt.GetStatus());
+                    SelectedPet(katt);
+                    break;
+                case "Pappegøye":
+                    Pappegøye pappegøye = new Pappegøye();
+                    Console.WriteLine(pappegøye.GetStatus());
+                    SelectedPet(pappegøye);
+                    break;                
+                case "Ørn":
+                    Ørn ørn = new Ørn();
+                    Console.WriteLine(ørn.GetStatus());
+                    SelectedPet(ørn);
+                    break;
+                case "Avslutt":
+                    programRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("Ikke gyldig");
+                    break;
+            }
         }
-        
+
     }
     
     static void SelectedPet(object pet)
     {
-        Console.WriteLine("Hva vil du gjøre? (Feed, Pet, GoToToilet)");
-        string command = Console.ReadLine();
-        if (pet is Katt katt)
+        while (true)
         {
-            switch (command)
+            Console.WriteLine("Hva vil du gjøre? (Feed, Pet, GoToToilet)");
+            Console.WriteLine("Trykk 'e' for å gå tilbake til hovedmeny");
+            string command = Console.ReadLine();
+            
+            if (command.ToLower() == "e")
             {
-                case "Feed":
-                    katt.Feed();
-                    Console.WriteLine(katt.GetStatus());
-                    break;
-                case "Pet":
-                    katt.Pet();
-                    break;
-                case "GoToToilet":
-                    katt.GoToToilet();
-                    break;
-                default: 
-                    Console.WriteLine("Ikke gjylding");
-                    break;
+                Console.WriteLine("Avslutter kjæledyrmenyen.");
+                break;
+            } 
+            if (pet is Katt katt)
+            {
+                switch (command)
+                {
+                    case "Feed":
+                        katt.Feed();
+                        Console.WriteLine(katt.GetStatus());
+                        break;
+                    case "Pet":
+                        katt.Pet();
+                        Console.WriteLine(katt.GetStatus());
+                        break;
+                    case "GoToToilet":
+                        katt.GoToToilet();
+                        Console.WriteLine(katt.GetStatus());
+                        break;
+                    default:
+                        Console.WriteLine("Ikke gyldig");
+                        break;
+                }
             }
-        }
-        else if (pet is Hund hund)
-        {
-            switch (command)
+            else if (pet is Hund hund)
             {
-                case "Feed":
-                    hund.Feed();
-                    break;
-                case "Pet":
-                    hund.Pet();
-                    break;
-                case "GoToToilet":
-                    hund.GoToToilet();
-                    break;
-                default: 
-                    Console.WriteLine("Ikke gjylding");
-                    break;
+                switch (command)
+                {
+                    case "Feed":
+                        hund.Feed();
+                        Console.WriteLine(hund.GetStatus());
+                        break;
+                    case "Pet":
+                        hund.Pet();
+                        Console.WriteLine(hund.GetStatus());
+                        break;
+                    case "GoToToilet":
+                        hund.GoToToilet();
+                        Console.WriteLine(hund.GetStatus());
+                        break;
+                    default:
+                        Console.WriteLine("Ikke gyldig");
+                        break;
+                }
+            }
+            else if (pet is Pappegøye pappegøye)
+            {
+                switch (command)
+                {
+                    case "Feed":
+                        pappegøye.Feed();
+                        Console.WriteLine(pappegøye.GetStatus());
+                        break;
+                    case "Pet":
+                        pappegøye.Pet();
+                        Console.WriteLine(pappegøye.GetStatus());
+                        break;
+                    case "GoToToilet":
+                        pappegøye.GoToToilet();
+                        Console.WriteLine(pappegøye.GetStatus());
+                        break;
+                    default:
+                        Console.WriteLine("Ikke gyldig");
+                        break;
+                }
+                
+            }
+            else if (pet is Ørn ørn)
+            {
+                switch (command)
+                {
+                    case "Feed":
+                        ørn.Feed();
+                        Console.WriteLine(ørn.GetStatus());
+                        break;
+                    case "Pet":
+                        ørn.Pet();
+                        Console.WriteLine(ørn.GetStatus());
+                        break;
+                    case "GoToToilet":
+                        ørn.GoToToilet();
+                        Console.WriteLine(ørn.GetStatus());
+                        break;
+                    default:
+                        Console.WriteLine("Ikke gyldig");
+                        break;
+                }
             }
         }
     }
